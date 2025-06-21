68f421473d4722ab6d51ede6907f9fba5085aec2
TomPancakes <tomrg2005@gmail.com>
2025-06-21 05:35:49 +0000

    [automatic commit] 2025-06-21 05:35:49 UTC 36.255.115.0

--- starthere/app.js
+++ starthere/app.js
@@ -31,7 +31,7 @@ let db;
       host: 'localhost',
       user: 'root',
       password: '',
-      database: 'testdb'
+      database: 'dogwalks'
     });
 
     // Create a table if it doesn't exist
